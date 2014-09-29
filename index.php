59f72f31f6a66cebb1b760b823e4fbf1609ca9a1
Eric <Eric@192.168.1.116>
2014-09-29 18:25:32 -0400

    test

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+      <form action="update.php" method="post">
+Enter your name: <input type="text" name="name">
+Enter your username: <input type="text" name="username">
+
+<input type="submit">
+</form>
+    </body>
+</html>
