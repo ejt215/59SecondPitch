6d4c5f729cdc7bfe1b390d4c4685eead319bd177
Enigmaxic <smittanator@gmail.com>
2014-10-02 17:40:39 -0400

    Trying to add a stylesheet with a background image

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title>59SecondPitch</title>
         <link href="CSS/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="CSS/newProfileStyles.css" rel="stylesheet">
     </head>
     <body>
         <script src="http://code.jquery.com/jquery.js"></script>
@@ -59,47 +60,49 @@ and open the template in the editor.
             $username = $_POST["username"];
         }
         ?>
-        <!--form setup taken from tutorial for twitter bootstrap-->
-        <form class="form-horizontal" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="control-group">
-                <label class="control-label" for="username">Username:</label>
-                <div class="controls"><input type="text" id="username" name="username" value = "<?php echo $username; ?>"></div>
-            </div>
-            <div class="control-group">
-                <label class="control-label" for="password">Password:</label>
-                <div class="controls"><input type="password" id="password" name="password" value = "<?php echo $password; ?>"><span class="error">* <?php echo $passwordErr; ?></span><br></div>
-            </div>
-            <div class="control-group">
-                <label class="control-label" for="repassword">Re-Enter Password:</label>
-                <div class="controls"><input type="password" id="repassword" name="repassword" value = "<?php echo $repassword; ?>"></div>
-            </div>
-            <div class="control-group">
-                <label class="control-label" for="email">Email:</label>
-                <div class="controls"><input type="text" id="email" name="email" value = "<?php echo $email; ?>"> <span class="error">* <?php echo $emailErr; ?></span></div>
-            </div>
-            <div class="control-group">
-                <label class="control-label" for="firstname">First Name:</label>
-                <div class="controls"><input type="text" id="firstname" name="firstname" value = "<?php echo $firstname; ?>"><span class="error">* <?php echo $firstnameErr; ?></span></div>
-            </div>
-            <div class="control-group">
-                <label class="control-label" for="lastname">Last Name:</label>
-                <div class="controls"><input type="text" id="lastname" name="lastname" value = "<?php echo $lastname; ?>"><span class="error">* <?php echo $lastnameErr; ?></span></div>
-            </div>
-            <div class="control-group">
-                <label class="control-label" for="age">Age:</label>
-                <div class="controls"><input type="text" id="age" name="age" value = "<?php echo $age; ?>"></div>
-            </div>
-            <div class="control-group">
-                <label class="control-label" for="almamater">University/Alma Mater:</label>
-                <div class="controls"><input type="text" id="almamater" name="almamater" value = "<?php echo $almamater; ?>"></div>
-            </div>
-            <div class="control-group">
-                <label class="control-label" for="city">City/Metro Area:</label>
-                <div class="controls"><input type="text" id="city" name="city" value = "<?php echo $city; ?>"></div>
-            </div>
-            <div class="control-group">
-                <div class="controls"><button type="submit" class="btn">Submit</button></div>
-            </div>
-        </form>
+        <div class="container">
+            <!--form setup taken from tutorial for twitter bootstrap-->
+            <form class="form-horizontal" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <div class="control-group">
+                    <label class="control-label" for="username">Username:</label>
+                    <div class="controls"><input type="text" id="username" name="username" value = "<?php echo $username; ?>"></div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="password">Password:</label>
+                    <div class="controls"><input type="password" id="password" name="password" value = "<?php echo $password; ?>"><span class="error">* <?php echo $passwordErr; ?></span><br></div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="repassword">Re-Enter Password:</label>
+                    <div class="controls"><input type="password" id="repassword" name="repassword" value = "<?php echo $repassword; ?>"></div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="email">Email:</label>
+                    <div class="controls"><input type="text" id="email" name="email" value = "<?php echo $email; ?>"> <span class="error">* <?php echo $emailErr; ?></span></div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="firstname">First Name:</label>
+                    <div class="controls"><input type="text" id="firstname" name="firstname" value = "<?php echo $firstname; ?>"><span class="error">* <?php echo $firstnameErr; ?></span></div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="lastname">Last Name:</label>
+                    <div class="controls"><input type="text" id="lastname" name="lastname" value = "<?php echo $lastname; ?>"><span class="error">* <?php echo $lastnameErr; ?></span></div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="age">Age:</label>
+                    <div class="controls"><input type="text" id="age" name="age" value = "<?php echo $age; ?>"></div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="almamater">University/Alma Mater:</label>
+                    <div class="controls"><input type="text" id="almamater" name="almamater" value = "<?php echo $almamater; ?>"></div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="city">City/Metro Area:</label>
+                    <div class="controls"><input type="text" id="city" name="city" value = "<?php echo $city; ?>"></div>
+                </div>
+                <div class="control-group">
+                    <div class="controls"><button type="submit" class="btn">Submit</button></div>
+                </div>
+            </form>
+        </div>
     </body>
 </html>
