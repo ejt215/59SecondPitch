ed2c00e725fed57c5e65bc543c1750d5c354ca81
Eric <Eric@dyn157253.cc.lehigh.edu>
2014-10-09 16:21:29 -0400

    password/repassword compare

--- update.php
+++ update.php
@@ -6,7 +6,7 @@
  * and open the template in the editor.
  */
 
-$con=mysqli_connect("127.0.0.1","root","");
+$con=mysqli_connect("162.242.219.56:3306","59App","AppAppDevDev?");
 
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
@@ -14,33 +14,33 @@ if (mysqli_connect_errno()) {
 
 
 
-$username = mysqli_real_escape_string($con, $_POST['username']);
-$password = mysqli_real_escape_string($con, $_POST['password']);
-$firstname = mysqli_real_escape_string($con, $_POST['firstname']);
-$lastname = mysqli_real_escape_string($con, $_POST['lastname']);
-$email = mysqli_real_escape_string($con, $_POST['email']);
-$age = mysqli_real_escape_string($con, $_POST['age']);
-$almamater = mysqli_real_escape_string($con, $_POST['almamater']);
-$city = mysqli_real_escape_string($con, $_POST['city']);
-$selected = mysqli_select_db($con,"59SecondPitch") 
+//$username = mysqli_real_escape_string($con, $_POST['username']);
+$password = mysqli_real_escape_string($con, trim($_POST['password']));
+$firstname = mysqli_real_escape_string($con, trim($_POST['firstname']));
+$lastname = mysqli_real_escape_string($con, trim($_POST['lastname']));
+$email = mysqli_real_escape_string($con, trim($_POST['email']));
+$age = mysqli_real_escape_string($con, trim($_POST['age']));
+$almamater = mysqli_real_escape_string($con, trim($_POST['almamater']));
+$city = mysqli_real_escape_string($con, trim($_POST['city']));
+$selected = mysqli_select_db($con,"59App") 
   or die("Could not select examples");
-$sql1 = "select email from investor where email=".$email;
+//$sql1 = "select email from investor where email=".$email;
 
 
-$result1 = mysqli_query($con,$sql1);
-$numRows = $result->num_rows;
-if($numRows >0){
+//$result1 = mysqli_query($con,$sql1);
+//$numRows = $result->num_rows;
+//if($numRows >0){
     
-}
+//}
 
-$sql="INSERT INTO investor (username,password,firstname,lastname,email,age,almamater,city)
-VALUES ('$username','$password','$firstname','$lastname','$email','$age','$almamater','$city')";
+$sql="INSERT INTO investor (email,password,firstname,lastname,age,almamater,city)
+VALUES ('$email','$password','$firstname','$lastname','$age','$almamater','$city')";
 
 if (!mysqli_query($con,$sql)) {
   die('Error: ' . mysqli_error($con));
 }
 
-$result = mysqli_query($con,"SELECT firstname,almamater FROM investor WHERE username ='".$username."'");
+$result = mysqli_query($con,"SELECT firstname,almamater FROM investor WHERE email ='".$email."'");
 //fetch tha data from the database
 while ($row = mysqli_fetch_array($result)) {
    echo "Hello ".$row{'firstname'}." your almamater is ".$row{'almamater'}; 
