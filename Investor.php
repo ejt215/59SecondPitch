3a2effea2b25977b51d704a46691ee5bed4533ff
Enigmaxic <smittanator@gmail.com>
2014-10-09 17:54:42 -0400

    Created investor sign up page

---
+++ Investor.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>59SecondPitch</title>
+        <link href="CSS/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="CSS/newProfileStyles.css" rel="stylesheet">
+        
+    </head>
+    <body>
+        <script src="http://code.jquery.com/jquery.js"></script>
+        <script src="JS/bootstrap.min.js"></script>
+        <script src="JS/myScript.js"></script>
+        
+        <div class="container">
+            <!--form setup taken from tutorial for twitter bootstrap-->
+            <form id="investorForm" class="form-horizontal" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <div class="control-group">
+                    <label class="control-label">How would you classify yourself?</label>
+                    <div class="controls">
+                        <input id="investorRadio" type="radio" name="userType" value="Investor">Investor<br>
+                        <input id="advisorRadio" type="radio" name = "userType" value="Advisor">Advisor<br>
+                        <input id="helpingHandRadio" type="radio" name="userType" value="helpingHand">Helping Hand
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label">How would you like to be contacted?</label>
+                    <div class="controls">
+                        <input id="phoneRadio" type="radio" name="contactType" value="Phone">Phone<br>
+                        <input id="emailRadio" type="radio" name = "contactType" value="Email">Email<br>
+                        <input id="eitherRadio" type="radio" name="contactType" value="Either">Either
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label">What are your contact preferences?(days,times,etc.)</label>
+                    <div class="controls"><textarea rows="4" cols="50"></textarea></div>
+                </div>
+            </form>
+        </div>
+    </body>
