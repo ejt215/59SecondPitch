db44ff9649ab7caedd9e8ca8e5e8ea3d9b304e46
Enigmaxic <smittanator@gmail.com>
2014-10-15 16:43:36 -0400

    Fixed style for investor, add entrepreneur page, changed update to connect to mysql database on my desktop

---
+++ Entrepreneur.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>59SecondPitch</title>
+        <link href="CSS/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="CSS/newProfileStyles.css" rel="stylesheet">
+    </head>
+    <body></body>
