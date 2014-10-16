b983b789380d0b8d13775623c8aa5300a6c2be67
Eric <Eric@dyn157131.cc.lehigh.edu>
2014-10-16 15:16:21 -0400

    fixed post problem + direct to investor/entrepreneur

--- update.php
+++ update.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,15 +14,18 @@ if (mysqli_connect_errno()) {
 
 
 
-$username = mysqli_real_escape_string($con, $_POST['username']);
-$password = mysqli_real_escape_string($con, trim($_POST['password']));
-$email = mysqli_real_escape_string($con, trim($_POST['email']));
-$firstname = mysqli_real_escape_string($con, trim($_POST['firstname']));
-$lastname = mysqli_real_escape_string($con, trim($_POST['lastname']));
-$age = mysqli_real_escape_string($con, trim($_POST['age']));
-$almamater = mysqli_real_escape_string($con, trim($_POST['almamater']));
-$city = mysqli_real_escape_string($con, trim($_POST['city']));
-//$type = mysqli_real_escape_string($con, trim($_POST['type']));
+//$username = mysqli_real_escape_string($con, filter_input(INPUT_POST, 'username'));
+$username = $_SESSION['username'];
+$password = $_SESSION['password'];
+$email = $_SESSION['email'];
+$firstname = $_SESSION['firstname'];
+
+
+$lastname = $_SESSION['password'];
+$age = $_SESSION['age'];
+$almamater = $_SESSION['almamater'];
+$city = $_SESSION['city'];
+$type = $_SESSION['type'];
 //$selected = mysqli_select_db($con,"59App") 
   //or die("Could not select examples");
 //$sql1 = "select email from investor where email=".$email;
@@ -40,21 +43,24 @@ VALUES ('$username','$password','$email','$firstname','$lastname','$age','$almam
 if (!mysqli_query($con,$sql)) {
   die('Error: ' . mysqli_error($con));
 }
-echo "Yeah Buddy";
-/*
+//echo "Yeah Buddy";
+
+
+
+
 if($type=="Investor"){
     header("Location: http://localhost/59SecondPitch/Investor.php"); 
 exit();
 }
-else{
+else if ($type =="Entrepreneur"){
     header("Location: http://localhost/59SecondPitch/Entrepreneur.php"); 
 exit();
 }
-*/
 
 
-/*
-$result = mysqli_query($con,"SELECT firstname,almamater FROM /* WHERE email ='".$email."'");
+
+
+/*$result = mysqli_query($con,"SELECT firstname,almamater FROM 59Profile WHERE email ='".$email."'");
 //fetch tha data from the database
 while ($row = mysqli_fetch_array($result)) {
    echo "Hello ".$row{'firstname'}." your almamater is ".$row{'almamater'}; 
