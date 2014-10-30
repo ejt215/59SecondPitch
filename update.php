4d6345ee7487b834d99dd81363f1a811d876db7b
Eric <Eric@192.168.1.104>
2014-10-30 16:11:49 -0400

    merged

--- update.php
+++ update.php
@@ -24,26 +24,7 @@ $dao = new fiftynineDAO();
 $sql="INSERT INTO 59Profile (username,password,email,firstname,lastname,age,almamater,city)
 VALUES ('$username','$password','$email','$firstname','$lastname','$age','$almamater','$city')";
 
-
-  
-  
-  
-  
-//}
-/*$sql1 = "select 59profileid from 59Profile where email = '".$email."'";
-$result = mysqli_query($con,$sql1);
-//fetch tha data from the database
-while ($row = mysqli_fetch_array($result)) {
-   $_SESSION['59profileid'] = $row{'59profileid'}; 
-   
-}
-//echo "Yeah Buddy";*/
-
-
-
-
 $dao->executeSQL($sql);
-
 
 if($type=="Investor"){
     header("Location: http://localhost/59SecondPitch/Investor.php"); 
@@ -53,4 +34,3 @@ else if ($type =="Entrepreneur"){
     header("Location: http://localhost/59SecondPitch/Entrepreneur.php"); 
     exit();
 }
-
