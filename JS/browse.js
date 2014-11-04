f39c2287904c0038105888b512b6b0f6ed297f9c
Maxwell Smith <smittanator@gmail.com>
2014-11-03 19:49:00 -0500

    Fixed Browse.php so it loads profiles when the document is ready

--- JS/browse.js
+++ JS/browse.js
@@ -3,6 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+function displayNewProfiles(data) {
+    var profile;
+    for (i = 1; i < 6; i++) {
+        profile = data["" + i];
+
+        $("#" + i).html(
+                "<h1> " + profile["business_name"] + "</h1><br />" +
+                "Business type: " + profile["business_type"] + "<br />" +
+                "Creator: " + profile["firstname"] + " " + profile["lastname"] + "<br />" +
+                "Almamater: " + profile['almamater'] + "<br />" +
+                "Location: " + +profile['city'] + "<br />" +
+                "Description :" + profile['business_description'] + "<br />" +
+                "<button type='button' class='viewProfile' id='b" + i + "'>View Full Profile</button>"
+                );
+    }
+}
 
 $(document).ready(function() {
     //initialize sidebar
@@ -15,44 +31,37 @@ $(document).ready(function() {
         innerOffset: 50,
         innerScale: .7
     });
-    
-    $('.viewProfile').click(function(){
+
+    $('.viewProfile').click(function() {
         $('#profileContainer').hide();
+    });
+
+    //Grab 5 profiles to start
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "fetchProfile.php",
+        //Set cover content to the 5 fetched profiles
+        success: function(data) {
+            displayNewProfiles(data);
+        }
     });
 
     //Next fetches 5 random profiles to display 
     $("#next").click(function() {
-
-
-        var clickBtnValue = $(this).val();
-
-        data = {'action': clickBtnValue};
-
-        //data = $(this).serialize() + "&" + $.param(data);
         $.ajax({
             type: "POST",
             dataType: "json",
             url: "fetchProfile.php",
-            data: data,
+            
             //Set cover content to the 5 fetched profiles
             success: function(data) {
-
-                var profile;
-                for (i = 1; i < 6; i++) {
-                    profile = data["" + i];
-
-                    $("#" + i).html(
-                            "<h1> " + profile["business_name"] + "</h1><br />Business type: " + profile["business_type"] + "<br />Creator: " + profile["firstname"] + " " + profile["lastname"] + "<br /> " +
-                            "Almamater: " + profile['almamater'] + "<br />Location: " + profile['city'] + "<br />" + "Description :" + profile['business_description']+"<br />"+
-                            "<button type='button' class='viewProfile' id=b"+i+">View Full Profile</button>"
-                            
-                            );
-
-                }
+                displayNewProfiles(data);
             }
         });
     });
 });
+
 
 
 
