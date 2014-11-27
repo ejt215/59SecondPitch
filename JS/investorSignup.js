31dcb85d7f4c6adfd833e7ad566fb06d08bf9212
Enigmaxic <smittanator@gmail.com>
2014-11-26 19:13:07 -0500

    Investor signup now includes optional phone number

---
+++ JS/investorSignup.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $("input[name='contacttype']").click(function() {
+        checkedID = $(this).attr("id");
+        if (checkedID === "phoneRadio" || checkedID === "eitherRadio")
+        {
+            $("#phoneNumber").css("visibility", "visible");
+        }
+        else {
+            $("#phoneNumber").css("visibility", "hidden");
+        }
+    });
+});
+
+
