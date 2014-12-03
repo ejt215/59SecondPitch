2d21a680d151db237a6ac377f2fe135fa86adbe0
Eric <Eric@192.168.1.126>
2014-12-03 12:27:18 -0500

    fixed issue with my videos

--- fetchProfile.php
+++ fetchProfile.php
@@ -38,7 +38,7 @@ while ($row = mysqli_fetch_array($result)) {
     $row2 = mysqli_fetch_array($result2);
     //echo json_encode($row2{'business_name'} . " " . $row2{'business_type'} . " " . $row2{'firstname'} . " " . $row2{'lastname'} . " " . $row2{'almamater'} . " " . $row2{'city'} . " " . $row2{'business_description'});
     //$file = new SplFileObject("");
-    $list['' . $count] = array('profileid' => $profileid,'business_id' => $business_id, 'business_name' => $row2{'business_name'}, 'business_type' => $row2{'business_type'}, 'firstname' => $row2{'firstname'}, 'lastname' => $row2{'lastname'}, 'almamater' => $row2{'almamater'}, 'city' => $row2{'city'},'shortDesc' =>$row2{'shortDesc'}, 'business_description' => $row2{'business_description'}, 'profilepicture' => $row2{'profilepicture'});
+    $list['' . $count] = array('profileid' => $profileid,'business_id' => $business_id, 'business_name' => $row2{'business_name'}, 'business_type' => $row2{'business_type'}, 'firstname' => $row2{'firstname'}, 'lastname' => $row2{'lastname'}, 'almamater' => $row2{'almamater'}, 'city' => $row2{'city'},'shortDesc' =>$row2{'shortDesc'}, 'business_description' => $row2{'business_description'}, 'profilepicture' => $row2{'profilepicture'}, 'business_video' => $row2{'business_video'});
 
     $count ++;
 }
